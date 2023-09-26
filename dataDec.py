99c6a47ef0bf371e4e5c3c745651963a55b3f511
VassilisAntonakakis <94934935+VassilisAntonakakis@users.noreply.github.com>
2023-09-27 02:53:32 +0300

    added prompt messages

--- dataDec.py
+++ dataDec.py
@@ -5,7 +5,7 @@ import numpy as np
 
 #check if the input file name is provided
 if len(sys.argv) < 2:
-    print("Usage: python dataEnc.py <input file name>")
+    print("[!]Usage: python dataEnc.py <input file name>")
     sys.exit(0)
 
 #get the input file name from the command line
@@ -13,33 +13,42 @@ inputFile = sys.argv[1]
 
 #locate the file in the computer
 if not os.path.isfile(inputFile):
-    print("File not found")
+    print("[-]File not found")
     sys.exit(0)
 else:
-    print("File found")
+    print("[+]File found")
     filepath = os.path.abspath(inputFile)
 
+#start timer
+start = cv.getTickCount()
+
+print("[!]Reading file")
 #read the file into a int array
 img = cv.imread(filepath, cv.IMREAD_GRAYSCALE)
 
+print("[!]Flattening the data")
 #convert the image into a 1d array
 gray_values = img.flatten()
 
+print("[!]Extracting header")
 #locate the header in the array and extract the file type
 header_start = np.where(gray_values == 36)[0][0]
 header_end = np.where(gray_values == 36)[0][1]
 file_type = gray_values[header_start+1:header_end].tobytes().decode()
 
-print("File type: " + file_type)
-
+print("[!]Removing header and leading zeros")
 #remove the header and the leading zeros from the array
 gray_values = gray_values[header_end+1:]
 gray_values = gray_values[np.where(gray_values != 0)[0][0]:]
 
+print("[!]Creating filename for the output")
 #create a new file with given filename with the "_enc" suffix replaced with "_dec" and the file type
 outputFile = inputFile.replace("_enc", "_dec")[:-3] + file_type
 
+print("[!]Writing to file")
 #write the data to the file
 file = open(outputFile, "wb")
 file.write(gray_values)
 file.close()
+
+print(f'[+]File saved as {outputFile}! Time taken: {(cv.getTickCount() - start) / cv.getTickFrequency()} s')
--- dataEnc.py
+++ dataEnc.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 #check if the input file name is provided
 if len(sys.argv) < 2:
-    print("Usage: python dataEnc.py <input file name>")
+    print("[!]Usage: python dataEnc.py <input file name>")
     sys.exit(0)
 
 #get the input file name from the command line
@@ -14,12 +14,16 @@ inputFile = sys.argv[1]
 
 #locate the file in the computer
 if not os.path.isfile(inputFile):
-    print("File not found")
+    print("[-]File not found")
     sys.exit(0)
 else:
-    print("File found")
+    print("[+]File found")
     filepath = os.path.abspath(inputFile)
 
+#start timer
+start = cv.getTickCount()
+
+print("[!]Reading file")
 file = open(filepath, "rb")
 
 #read the file into a byte array
@@ -30,10 +34,11 @@ file.close()
 
 gray_values = np.frombuffer(data, dtype=np.uint8)
 
-
+print("[!]Calculating size of the image")
 #calculate the size of the equivalent 2d array
 size = int(np.sqrt(len(gray_values)))
 
+print("[!]Creating image header")
 #create a header for the image with the original file type preceded and followed by $
 header = "$" + inputFile[-3:] + "$"
 
@@ -43,33 +48,35 @@ header_values = np.frombuffer(header.encode(), dtype=np.uint8)
 #add the header to the start of the array
 gray_values = np.insert(gray_values, 0, header_values)
 
+print("[!]Adjusting size of the image")
 while size*size < len(gray_values):
     size += 1
 
-print("data from file: " + str(gray_values))
-
+print("[!]Adding zeros to fill the blank spaces")
 #add zeros to the start of the array to make it a perfect square
+zeros_to_add = size*size - len(gray_values)
+zeros_added = 0
 while len(gray_values) < size*size:
+    print(f'\r[!]Zeros added: {zeros_added}/{zeros_to_add}', end='')
     gray_values = np.insert(gray_values, 0, 0)
+    zeros_added += 1
 
-print("data after adding zeros: " + str(gray_values))
+print("\n[!]Reshaping the array into a 2d array")
 #reshape the 1d array into a 2d array
 gray_img = gray_values.reshape(size, size)
 
-print("Image: " + str(gray_img))
+#stop timer
+end = cv.getTickCount() - start
 
-print("Image size: " + str(gray_img.shape))
 #check if the image is created
 if gray_img is None:
-    print("Image not created")
+    print(f'[-]Image not created! Time taken: {str(end/cv.getTickFrequency())}s')
     sys.exit(0)
 else:
-    print("Image created")
-    print("image: " + str(gray_img))
+    print(f'[+]Image created successfully with size of {str(pow(size, 2))} bytes! Time taken: {str(end/cv.getTickFrequency())}s')
     #save the image as a tiff file with the file name from the command line and the adittion of "_enc.tif"
     cv.imwrite(inputFile[:-4] + "_enc.tif", gray_img)
 
 #show the image in a window with matplotlib
 plt.imshow(gray_img, cmap='gray', vmin=0, vmax=255)
 plt.show()
-
